cfdc4d8c93904edba08008bda2ca90c5705c1108
Arquimedes I <aguiland06@gmail.com>
2023-10-14 10:16:04 -0400

    add list of bitacoras fail

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               </div>
               <nav className="ml-auto text-sm font-medium space-x-6">
                 <Link href="/">Home</Link>
+                <Link href="/bitacora/">List</Link>
                 <Link href="/about">About</Link>
                 <Link href="/protected">Login</Link>
               </nav>
