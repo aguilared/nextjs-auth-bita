9ab8c5b48370ceb4515742c3b34c59e3cce01547
aguilared <arquimedesgui@gmail.com>
2023-10-18 06:59:34 -0400

    add tanckstack-table

--- app/bitacora/page.tsx
+++ app/bitacora/page.tsx
@@ -1,70 +1,54 @@
-/* eslint-disable react/display-name */
 "use client";
-import { useState, forwardRef } from "react";
-import PropTypes from "prop-types";
-import { withStyles, makeStyles } from "@mui/styles";
-import Modal from "@mui/material/Modal";
-import { red } from "@mui/material/colors";
-import dayjs from "dayjs";
-import useSWR from "swr";
-//import BitacoraCard from "../../components/Bitacoras/BitacoraCard";
-import AddBox from "@mui/material/Icon";
-import ArrowDownward from "@mui/material/Icon";
-import Check from "@mui/material/Icon";
-import ChevronLeft from "@mui/material/Icon";
-import ChevronRight from "@mui/material/Icon";
-import Clear from "@mui/material/Icon";
-import DeleteOutline from "@mui/material/Icon";
-import Edit from "@mui/material/Icon";
-import FilterList from "@mui/material/Icon";
-import FirstPage from "@mui/material/Icon";
-import LastPage from "@mui/material/Icon";
-import Remove from "@mui/material/Icon";
-import SaveAlt from "@mui/material/Icon";
-import Search from "@mui/material/";
-import ViewColumn from "@mui/material/Icon";
-
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
-
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import axios from "axios";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import DebouncedInput from "@/components/DebouncedInput";
+import { SearchIcon } from "@/components/Icons/Icons";
 
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "bitacora_date", headerName: "Date", width: 130 },
-  { field: "author_id", headerName: "Author", width: 130 },
-  { field: "_count", headerName: "Totals", width: 130 },
-  {
-    field: "_count.bita_events",
-    headerName: "Events",
-    type: "number",
-    width: 130,
-  },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params: GridValueGetterParams) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
+const TanStackTable = () => {
+  const columnHelper = createColumnHelper();
 
-const BitaEvents = (props: any): JSX.Element => {
+  const columns = [
+    columnHelper.accessor("", {
+      id: "item",
+      cell: (info) => <span>{info.row.index + 1}</span>,
+      header: "Item",
+    }),
+
+    columnHelper.accessor("id", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Id",
+    }),
+    columnHelper.accessor("bitacora_date", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Date",
+    }),
+    columnHelper.accessor("author_id", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Author",
+    }),
+    columnHelper.accessor("_count.bita_events", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Total",
+    }),
+    columnHelper.accessor("visits", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Visits",
+    }),
+    columnHelper.accessor("progress", {
+      cell: (info) => <span>{info.getValue()}</span>,
+      header: "Progress",
+    }),
+  ];
+
   const { status, data, error, isLoading, refetch } = useQuery(
     ["bitacoras"],
     async () => {
@@ -76,44 +60,139 @@ const BitaEvents = (props: any): JSX.Element => {
 
   console.log("Data", data);
 
+  const [globalFilter, setGlobalFilter] = useState("");
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      globalFilter,
+    },
+    getFilteredRowModel: getFilteredRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
   return (
-    <div className="flex rounded items-center justify-center flex-wrap p-2">
-      <div className="shadow-lg ">
+    <div className="pt-4 min-h-screen bg-gray-900">
+      <div className="p-2 max-w-5xl mx-auto fill-gray-400">
         {!data ? (
           <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
           </div>
         ) : (
-          <DataGrid
-            rows={data}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: { page: 0, pageSize: 5 },
-              },
-            }}
-            pageSizeOptions={[5, 10]}
-            checkboxSelection
-          />
+          <div className="flex justify-between mb-2">
+            <div className="w-full flex items-center gap-1">
+              <SearchIcon />
+              <DebouncedInput
+                value={globalFilter ?? ""}
+                onChange={(value) => setGlobalFilter(String(value))}
+                className="p-2 bg-transparent outline-none border-b-2 w-1/5 focus:w-1/3 duration-300 border-indigo-500"
+                placeholder="Search all columns..."
+              />
+            </div>
+            <table className="border border-gray-700 w-full text-left">
+              <thead className="bg-indigo-600">
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <tr key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <th key={header.id} className="capitalize px-3.5 py-2">
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </th>
+                    ))}
+                  </tr>
+                ))}
+              </thead>
+              <tbody>
+                {table.getRowModel().rows.length ? (
+                  table.getRowModel().rows.map((row, i) => (
+                    <tr
+                      key={row.id}
+                      className={`
+                ${i % 2 === 0 ? "bg-gray-900" : "bg-gray-800"}
+                `}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <td key={cell.id} className="px-3.5 py-2">
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      ))}
+                    </tr>
+                  ))
+                ) : (
+                  <tr className="text-center h-32">
+                    <td colSpan={12}>No Recoard Found!</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+            {/* pagination */}
+            <div className="flex items-center justify-end mt-2 gap-2">
+              <button
+                onClick={() => {
+                  table.previousPage();
+                }}
+                disabled={!table.getCanPreviousPage()}
+                className="p-1 border border-gray-300 px-2 disabled:opacity-30"
+              >
+                {"<"}
+              </button>
+              <button
+                onClick={() => {
+                  table.nextPage();
+                }}
+                disabled={!table.getCanNextPage()}
+                className="p-1 border border-gray-300 px-2 disabled:opacity-30"
+              >
+                {">"}
+              </button>
+
+              <span className="flex items-center gap-1">
+                <div>Page</div>
+                <strong>
+                  {table.getState().pagination.pageIndex + 1} of{" "}
+                  {table.getPageCount()}
+                </strong>
+              </span>
+              <span className="flex items-center gap-1">
+                | Go to page:
+                <input
+                  type="number"
+                  defaultValue={table.getState().pagination.pageIndex + 1}
+                  onChange={(e) => {
+                    const page = e.target.value
+                      ? Number(e.target.value) - 1
+                      : 0;
+                    table.setPageIndex(page);
+                  }}
+                  className="border p-1 rounded w-16 bg-transparent"
+                />
+              </span>
+              <select
+                value={table.getState().pagination.pageSize}
+                onChange={(e) => {
+                  table.setPageSize(Number(e.target.value));
+                }}
+                className="p-2 bg-transparent"
+              >
+                {[10, 20, 30, 50].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
         )}
       </div>
     </div>
   );
 };
 
-const styles = {
-  card: {
-    maxWidth: 645,
-  },
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 40,
-  },
-  avatar: {
-    backgroundColor: red[500],
-  },
-};
-
-export default BitaEvents;
+export default TanStackTable;
